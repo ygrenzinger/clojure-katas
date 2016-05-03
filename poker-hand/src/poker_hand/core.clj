2ece0291ff96feeca1812553dd264da9a87c91ad
ygrenzinger <yannick.grenzinger@gmail.com>
2016-05-03 13:37:08 +0200

    Complete Kata but maybe to complete and complex. Some case tested in XXX-winner? can't happen.

--- poker-hand/src/poker_hand/core.clj
+++ poker-hand/src/poker_hand/core.clj
@@ -32,6 +32,7 @@
                   :king  11,
                   :ace   12})
 
+;parsing functions
 (defn parse-card [value]
   {:suit (get suit-char (nth value 1)),
    :rank (get rank-char (nth value 0))})
@@ -39,58 +40,215 @@
 (defn parse-cards [value]
   (map parse-card (clojure.string/split value #" ")))
 
-(defn parse-player-hand [player-hand]
-  {:name (key player-hand) :cards (parse-cards (val player-hand))})
-
-(defn associate-card-to-player [player-hand]
-  (zipmap (:cards player-hand) (repeat (:name player-hand))))
-
 (defn parse-hands [hands]
-  (let [nb-of-players (count hands)
-        parsed-hands (map parse-player-hand hands)
-        cards-associated-to-player (into {} (apply concat (map associate-card-to-player parsed-hands)))]
-    (if (not= (* 5 nb-of-players) (count cards-associated-to-player))
-      (throw (Exception. "Some players have the same cards !"))
-      cards-associated-to-player)
-    )
-  )
+  (into {} (map (fn [[player hands-not-parsed]] [player (parse-cards hands-not-parsed)]) hands)))
 
-(defn highest-card [hands]
-  (let [grouped-by-rank (group-by #(:rank (key %)) hands)
+;Winning figure functions
+(defn associate-cards-to-player [hands-parsed]
+  (into {} (apply concat (map (fn [hand] (zipmap (val hand) (repeat (key hand)))) hands-parsed))))
+
+(defn highest-player-card? [cards-associated-to-player]
+  (let [grouped-by-rank (group-by #(:rank (key %)) cards-associated-to-player)
         filtered-for-one-card-by-rank (filter #(= 1 (count (val %))) grouped-by-rank)]
     (if (empty? filtered-for-one-card-by-rank)
       nil
       (let [[_ [max]] (apply max-key #(rank-values (key %)) filtered-for-one-card-by-rank)]
         {:name (val max) :card (key max)}))))
 
+(defn highest-player-card-winner? [hands]
+  (let [highest-player-card (highest-player-card? (associate-cards-to-player hands))]
+    (if (nil? highest-player-card)
+      "Draw"
+      (str (:name highest-player-card) " wins with highest card " (:rank (:card highest-player-card)) " of " (:suit (:card highest-player-card))))))
+
 (defn count-rank [cards]
-  (frequencies (map #(:rank (nth % 0)) cards)))
+  (frequencies (map #(:rank %) cards)))
 
-(defn find-multiple-rank [grouped-by-player multiple]
-  (map (fn [hand] [(key hand) (filter #(= multiple (second %)) (count-rank (val hand)))]) grouped-by-player))
+(def memoized-count-rank (memoize count-rank))
 
-(defn winning-multiple-rank [hands multiple]
-  (let [grouped-by-player (group-by val hands)
-        hands-with-pair (filter #(not-empty (second %)) (find-multiple-rank grouped-by-player multiple))]
-    (if (empty? hands-with-pair)
-      []
-      (let [pair-by-player (into {} (map (fn [[k v]] [k (first (first v))]) hands-with-pair))
-            pair-sorted-by-card-values (reverse (sort (group-by #(rank-values (val %)) pair-by-player)))]
-        (second (first pair-sorted-by-card-values))))))
+(defn sort-by-rank-decreasing [cards]
+  (reverse (sort-by #(rank-values (:rank %)) cards)))
 
-(defn winning-hand [hands]
-  (let [hands-parsed (parse-hands hands)]
-    (let [winning-pairs (winning-multiple-rank hands-parsed 2)
-          winning-card (highest-card hands-parsed)]
-      (cond
-        (= 1 (count winning-pairs)) (str (first (first winning-pairs)) " has win the game with pair of " (second (first winning-pairs)))
-        (not (nil? winning-card)) (str (:name winning-card) " has win the game with highest card " (:rank (:card winning-card)) " of " (:suit (:card winning-card)))
-        :else "Draw")
-      )
-    )
-  )
+(defn find-multiple-rank [counted-rank multiple]
+  (filter #(= multiple (val %)) counted-rank))
+
+(defn multiple-of-a-kind? [cards multiple]
+  (let [counted-rank (memoized-count-rank cards)
+        multiple-of-a-kind (find-multiple-rank counted-rank multiple)]
+    (if-not (empty? multiple-of-a-kind)
+      (let [[[rank _]] multiple-of-a-kind]
+        {:rank rank, :remaining-cards (filter #(not= rank (:rank %)) cards)}))))
+
+(defn one-pair? [cards]
+  (multiple-of-a-kind? cards 2))
+
+(defn three-of-a-kind? [cards]
+  (multiple-of-a-kind? cards 3))
+
+(defn four-of-a-kind? [cards]
+  (multiple-of-a-kind? cards 4))
+
+(defn double-pair? [cards]
+  (let [counted-rank (memoized-count-rank cards)
+        pairs (find-multiple-rank counted-rank 2)]
+    (if (= 2 (count pairs))
+      (let [rank (vec (reverse (sort-by #(rank-values %) (map #(first %) pairs))))]
+        {:rank rank, :remaining-cards (filter #(not (some #{(:rank %)} rank)) cards)}))))
+
+(defn full? [cards]
+  (let [pair (one-pair? cards)
+        three-of-a-kind (three-of-a-kind? cards)]
+    (if (and three-of-a-kind pair) [(:rank three-of-a-kind) (:rank pair)])))
+
+(defn flush? [cards]
+  (if (apply = (map #(:suit %) cards)) (first (sort-by-rank-decreasing cards))))
+
+(defn straight? [cards]
+  (let [sorted-cards (sort-by-rank-decreasing cards)
+        highest-card (first sorted-cards)
+        highest-value (rank-values (:rank highest-card))]
+    (if (= (take 5 (iterate dec highest-value)) (map #(rank-values (:rank %)) sorted-cards))
+      highest-card)))
+
+(defn straight-flush? [cards]
+  (let [flush-card (flush? cards)
+        straight-card (straight? cards)]
+    (if (= flush-card straight-card)
+      flush-card)))
+
+(defn winning-with-full-hand-figure? [f figure hands]
+  (let [having-special-hand (filter #(second %) (map (fn [[k v]] [k (f v)]) hands))]
+    (cond
+      (> (count having-special-hand) 1) (let [grouped-by-rank (group-by #(rank-values (:rank (second %))) having-special-hand)
+                                              highest-ranks (apply max-key key grouped-by-rank)]
+                                          (str "Winning with " figure " : " (apply str (interpose ", " (map #(str (first %) " with " (:rank (second %)) " of " (:suit (second %))) (val highest-ranks))))))
+      (= 1 (count having-special-hand)) (let [[[player card]] having-special-hand] (str player " wins with a " figure " of " (:suit card) " at rank " (:rank card)))
+      :else nil)))
+
+(defn flush-winner? [hands]
+  (winning-with-full-hand-figure? flush? "flush" hands))
+
+(defn straight-winner? [hands]
+  (winning-with-full-hand-figure? straight? "straight" hands))
+
+(defn straight-flush-winner? [hands]
+  (winning-with-full-hand-figure? straight-flush? "straight flush" hands))
+
+(defn multiple-of-a-kind-winner? [f figure hands]
+  (let [having-special-hand (filter #(second %) (map (fn [[k v]] [k (f v)]) hands))]
+    (cond
+      (= 1 (count having-special-hand)) (let [[[player result]] having-special-hand] (str player " wins with a " figure " of " (:rank result)))
+      (> (count having-special-hand) 1) (let [grouped-by-rank (group-by #(rank-values (:rank (second %))) having-special-hand)
+                                              highest-ranks (apply max-key key grouped-by-rank)
+                                              winners (second highest-ranks)]
+                                          (if (= 1 (count winners))
+                                            (str (first (first winners)) " wins with a " figure " of " (:rank (second (first winners))))
+                                            (let [remaining-cards (into {} (map (fn [winner] [(first winner) (:remaining-cards (second winner))]) winners))
+                                                  highest-cards (highest-player-card? (associate-cards-to-player remaining-cards))]
+                                              (if (nil? highest-cards)
+                                                (str (apply str (interpose " and " (map #(first %) winners))) " win with a " figure " of " (:rank (second (first winners))))
+                                                (str (:name highest-cards) " wins with a " figure " of " (:rank (second (first winners))) " and highest card " (:rank (:card highest-cards)) " of " (:suit (:card highest-cards))))
+                                              )
+                                            ))
+      :else nil)))
+
+(defn four-of-a-kind-winner? [hands]
+  (multiple-of-a-kind-winner? four-of-a-kind? "four of kind" hands))
+
+(defn three-of-a-kind-winner? [hands]
+  (multiple-of-a-kind-winner? three-of-a-kind? "three of kind" hands))
+
+(defn pair-winner? [hands]
+  (multiple-of-a-kind-winner? one-pair? "pair" hands))
+
+(defn full-winner? [hands]
+  (let [having-special-hand (filter #(second %) (map (fn [[k v]] [k (full? v)]) hands))]
+    (cond
+      (= (count having-special-hand) 1) (str (first (first having-special-hand)) " wins with a full at " (first (second (first having-special-hand))) " and " (second (second (first having-special-hand))))
+      (> (count having-special-hand) 1) (let [grouped-by-three-of (group-by #(rank-values (first (second %))) having-special-hand)
+                                              highest-ranks-three-of (apply max-key key grouped-by-three-of)
+                                              winners-three-of (second highest-ranks-three-of)]
+                                          (if (= 1 (count winners-three-of))
+                                            (let [[[winner [rank-three-of rank-pair-of]]] winners-three-of]
+                                              (str winner " wins with a full of tree " rank-three-of " and two " rank-pair-of))
+                                            (let [grouped-by-two-of (group-by #(rank-values (second (second %))) winners-three-of)
+                                                  highest-ranks-two-of (apply max-key key grouped-by-two-of)
+                                                  winners-two-of (second highest-ranks-two-of)]
+                                              (if (= 1 (count winners-two-of))
+                                                (let [[[winner [rank-three-of rank-pair-of]]] winners-two-of]
+                                                  (str winner " wins with a full of tree " rank-three-of " and two " rank-pair-of))
+                                                (let [[[_ [rank-three-of rank-pair-of]]] winners-two-of]
+                                                  (str (apply str (interpose " and " (map #(first %) winners-two-of))) " win with a full of three " rank-three-of " and two " rank-pair-of)))
+                                              )))
+      :else nil)))
+
+(defn double-pair-winner? [hands]
+  (let [having-special-hand (filter #(second %) (map (fn [[k v]] [k (double-pair? v)]) hands))]
+    (cond
+      (= (count having-special-hand) 1) (let [[[winner {[pair1 pair2] :rank}]] having-special-hand]
+                                          (str winner " wins with double pair of " pair1 " and " pair2))
+      (> (count having-special-hand) 1) (let [grouped-by-rank-pair1 (group-by #(rank-values (first (:rank (second %)))) having-special-hand)
+                                              highest-ranks-pair1 (apply max-key key grouped-by-rank-pair1)
+                                              winners-pair1 (second highest-ranks-pair1)]
+                                          (if (= 1 (count winners-pair1))
+                                            (let [[[winner {[pair1 pair2] :rank}]] winners-pair1]
+                                              (str winner " wins with double pair of " pair1 " and " pair2))
+                                            (let [grouped-by-rank-pair2 (group-by #(rank-values (second (:rank (second %)))) winners-pair1)
+                                                  highest-ranks-pair2 (apply max-key key grouped-by-rank-pair2)
+                                                  winners-pair2 (second highest-ranks-pair2)]
+                                              (if (= 1 (count winners-pair2))
+                                                (let [[[winner {[pair1 pair2] :rank}]] winners-pair2]
+                                                  (str winner " wins with double pair of " pair1 " and " pair2))
+
+                                                (let [remaining-cards (into {} (map (fn [winner] [(first winner) (:remaining-cards (second winner))]) winners-pair2))
+                                                      highest-cards (highest-player-card? (associate-cards-to-player remaining-cards))]
+                                                  (let [[[_ {[pair1 pair2] :rank}]] winners-pair2]
+                                                    (if (nil? highest-cards)
+                                                      (str (apply str (interpose " and " (map #(first %) winners-pair2))) " win with a double pair of " pair1 " and " pair2)
+                                                      (str (:name highest-cards) " wins with a double pair of " pair1 " and " pair2 " and highest card " (:rank (:card highest-cards)) " of " (:suit (:card highest-cards)))))
+                                                  )
+                                                )
+                                              )))
+      :else nil)))
+
+(defn winner? [hands]
+  (let [winning-cases [straight-flush-winner?
+                       four-of-a-kind-winner?
+                       full-winner?
+                       flush-winner?
+                       straight-winner?
+                       three-of-a-kind-winner?
+                       double-pair-winner?
+                       pair-winner?
+                       highest-player-card-winner?]
+        parsed-hands (parse-hands hands)]
+    (first (drop-while #(nil? %) (map (fn [win?] (win? parsed-hands)) winning-cases)))))
+
+; (get-random-hand-for-players [player1 player2 player3])
+(defn get-random-hand-for-players [players]
+  (let [random-cards (shuffle (for [suit-char (keys suit-char)
+                                    rank-char (keys rank-char)]
+                                (str rank-char suit-char)))]
+    (loop [pool-of-cards random-cards
+           remaining-players players
+           game {}]
+      (if (empty? remaining-players)
+        game
+        (recur (drop 5 pool-of-cards)
+               (rest remaining-players)
+               (into game {(first remaining-players), (apply str (interpose " " (take 5 pool-of-cards)))})
+               )))))
+
+(defn play-random-game [players]
+  (let [hands (get-random-hand-for-players players)]
+    {:hands hands, :winner (winner? hands)}))
+
+(defn print-game [game]
+  (println "Game = " (:hands game))
+  (println "Winner = " (:winner game))
+  (println ""))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (doall (map print-game (repeatedly 20 #(play-random-game ["Sylvain" "Bruno" "Yannick" "Diego"])))))
--- poker-hand/test/poker_hand/core_test.clj
+++ poker-hand/test/poker_hand/core_test.clj
@@ -21,22 +21,151 @@
             {:suit :diamond, :rank :queen},
             {:suit :club, :rank :8}]))))
 
+(deftest special-hands-test
+  (testing "pair ?"
+    (is (= {:rank :8, :remaining-cards [{:suit :spade, :rank :ace} {:suit :club, :rank :3} {:suit :diamond, :rank :queen}]}
+           (one-pair? (parse-cards "AS 8H 3C QD 8C")))))
+  (testing "double pair ?"
+    (is (= {:rank [:ace :8], :remaining-cards [{:suit :club, :rank :3}]} (double-pair? (parse-cards "AS 8H 3C 8D AC")))))
+  (testing "three of a kind ?"
+    (is (= {:rank :3, :remaining-cards [{:suit :spade, :rank :ace} {:suit :club, :rank :8}]} (three-of-a-kind? (parse-cards "AS 3H 3C 3D 8C")))))
+  (testing "four of a kind ?"
+    (is (= {:rank :ace, :remaining-cards [{:suit :club, :rank :8}]} (four-of-a-kind? (parse-cards "AS AH AC AD 8C")))))
+  (testing "full"
+    (is (= [:2 :queen] (full? (parse-cards "QS 2H QC 2D 2C")))))
+  (testing "flush ?"
+    (is (= {:suit :diamond, :rank :king} (flush? (parse-cards "KD QD 3D 2D 8D")))))
+  (testing "straight ?"
+    (is (= {:suit :heart, :rank :6}) (straight? (parse-cards "2S 6H 3D 4H 5D")))
+    (is (= nil) (straight? (parse-cards "2S 6H 8D 4H 5D"))))
+  (testing "straight flush"
+    (is (= {:suit :heart, :rank :jack}) (straight-flush? (parse-cards "JD 9D TD 7D 8D")))))
+
 (deftest winning-hand-test
+  (testing "winning straight flush ?"
+    (is (= nil (straight-flush-winner? (parse-hands {player1 "AD KD QD JD TS", player2 "AD KD QD JD TH"}))))
+    (is (= "Player 1 wins with a straight flush of :diamond at rank :ace"
+           (straight-flush-winner? (parse-hands {player1 "AD KD QD JD TD", player2 "AD KD QD JD TH"}))))
+    (is (= "Winning with straight flush : Player 2 with :ace of :heart"
+           (straight-flush-winner? (parse-hands {player1 "9D KD QD JD TD", player2 "AH KH QH JH TH"}))))
+    (is (= "Winning with straight flush : Player 1 with :ace of :diamond, Player 2 with :ace of :heart"
+           (straight-flush-winner? (parse-hands {player1 "AD KD QD JD TD", player2 "AH KH QH JH TH"})))))
 
-  (testing "incorrect poker game !"
-    (is (thrown-with-msg? Exception
-                          #"Some players have the same cards !"
-                          (winning-hand {player1 "AS QD 3D TD 5S", player2 "AS QH 3C TH 9C"}))))
+  (testing "winning flush ?"
+    (is (= nil (flush-winner? (parse-hands {player1 "AD KD 5D JD TS", player2 "AD KD 5D JD TH"}))))
+    (is (= "Player 1 wins with a flush of :diamond at rank :ace"
+           (flush-winner? (parse-hands {player1 "AD KD 5D JD TD", player2 "AD KD 5D JD TH"}))))
+    (is (= "Winning with flush : Player 2 with :king of :heart"
+           (flush-winner? (parse-hands {player1 "9D 7D 5D JD TD", player2 "8H KH 5H JH TH"}))))
+    (is (= "Winning with flush : Player 1 with :king of :diamond, Player 2 with :king of :heart"
+           (flush-winner? (parse-hands {player1 "9D KD 5D JD TD", player2 "8H KH 5H JH TH"})))))
 
-  (testing "Winning hand with highest card"
-    (is (= (winning-hand {
-                          player1 "AS QD 3D TD 5S",
-                          player2 "AC QH 3C TH 9C",
-                          player3 "AD QC 5H JH 7C"
-                          })
-           "Player 3 has win the game with highest card :jack of :heart")))
+  (testing "winning straight ?"
+    (is (= nil (straight-winner? (parse-hands {player1 "AD KS QD JD 5S", player2 "AD KD 6D JD TC"}))))
+    (is (= "Player 1 wins with a straight of :diamond at rank :ace"
+           (straight-winner? (parse-hands {player1 "AD KD QD JD TD", player2 "5D KD QD JD TH"}))))
+    (is (= "Winning with straight : Player 2 with :ace of :heart"
+           (straight-winner? (parse-hands {player1 "9C KD QH JD TS", player2 "AH KS QD JS TD"}))))
+    (is (= "Winning with straight : Player 1 with :ace of :spade, Player 2 with :ace of :club"
+           (straight-winner? (parse-hands {player1 "AS KS QD JH TC", player2 "AC KS QC JC TD"})))))
 
-  (testing "Winning hand with pair"
-    (is (= (winning-hand {player1 "2S QD 5D 3D 5S", player2 "AC QH 3C TH 9C"})
-           "Player 1 has win the game with pair of :5")))
+  (testing "winning four of a kind ?"
+    (is (= nil (four-of-a-kind-winner? (parse-hands {player1 "4D AS AC AH 5S", player2 "4D AS AC AH 7H"}))))
+    (is (= "Player 1 wins with a four of kind of :ace"
+           (four-of-a-kind-winner? (parse-hands {player1 "AD AS AC AH 5S", player2 "AD AS 4C AH 7H"}))))
+    (is (= "Player 2 wins with a four of kind of :queen"
+           (four-of-a-kind-winner? (parse-hands {player1 "8D 8S 8C 8H 5S", player2 "QD QS QC QH 7H"}))))
+    (is (= "Player 2 wins with a four of kind of :ace and highest card :7 of :heart"
+           (four-of-a-kind-winner? (parse-hands {player1 "AD AS AC AH 5S", player2 "AD AS AC AH 7H"}))))
+    (is (= "Player 1 and Player 2 win with a four of kind of :ace"
+           (four-of-a-kind-winner? (parse-hands {player1 "AD AS AC AH 7S", player2 "AD AS AC AH 7H"})))))
+
+  (testing "winning three of a kind ?"
+    (is (= nil (three-of-a-kind-winner? (parse-hands {player1 "4D AS 2C AH 5S", player2 "4D AS AC 2H 7H"}))))
+    (is (= "Player 1 wins with a three of kind of :ace"
+           (three-of-a-kind-winner? (parse-hands {player1 "AD 2S AC AH 5S", player2 "AD 2S 4C AH 7H"}))))
+    (is (= "Player 2 wins with a three of kind of :queen"
+           (three-of-a-kind-winner? (parse-hands {player1 "8D 2S 8C 8H 5S", player2 "QD 2S QC QH 7H"}))))
+    (is (= "Player 2 wins with a three of kind of :ace and highest card :7 of :heart"
+           (three-of-a-kind-winner? (parse-hands {player1 "AD AS 2C AH 5S", player2 "AD AS 2C AH 7H"}))))
+    (is (= "Player 1 and Player 2 win with a three of kind of :ace"
+           (three-of-a-kind-winner? (parse-hands {player1 "AD AS 2C AH 7S", player2 "AD AS 2D AH 7H"})))))
+
+  (testing "winning pair ?"
+    (is (= nil (pair-winner? (parse-hands {player1 "4D 6S 2C AH 5S", player2 "4D 8S AC 2H 7H"}))))
+    (is (= "Player 1 wins with a pair of :ace"
+           (pair-winner? (parse-hands {player1 "AD 2S AC 7H 5S", player2 "AD 3S 4C 8C 7H"}))))
+    (is (= "Player 2 wins with a pair of :queen"
+           (pair-winner? (parse-hands {player1 "8D 2S 8C 3H 5S", player2 "QD 2S QC 5H 7H"}))))
+    (is (= "Player 2 wins with a pair of :ace and highest card :7 of :heart"
+           (pair-winner? (parse-hands {player1 "AD AS 2D 3H 5S", player2 "AD AS 2C 3D 7H"}))))
+    (is (= "Player 1 and Player 2 win with a pair of :ace"
+           (pair-winner? (parse-hands {player1 "AD AS 2C 5H 7S", player2 "AD AS 2D 5D 7H"})))))
+
+  (testing "winning full house ?"
+    (is (= nil (full-winner? (parse-hands {player1 "AS AC AD 2D 3D", player2 "AS 2C AD 3S 3D"}))))
+    (is (= "Player 1 wins with a full at :ace and :3"
+           (full-winner? (parse-hands {player1 "AS AC AD 3D 3D", player2 "AS 2C AD 3S 3D"}))))
+    (is (= "Player 1 wins with a full of tree :ace and two :3"
+           (full-winner? (parse-hands {player1 "AS AC AD 3D 3D", player2 "QS QC QD 8S 8D"}))))
+    (is (= "Player 2 wins with a full of tree :ace and two :8"
+           (full-winner? (parse-hands {player1 "AS AC AD 3H 3D", player2 "AS AC AD 8S 8D"}))))
+    (is (= "Player 1 and Player 2 win with a full of three :ace and two :3"
+           (full-winner? (parse-hands {player1 "AS AC AD 3H 3D", player2 "AS AC AD 3S 3D"})))))
+
+  (testing "winning with two pair ?"
+    (is (= nil (double-pair-winner? (parse-hands {player1 "AS 6C AD 2D 3D", player2 "AS 2C AD 8S 3D"}))))
+    (is (= "Player 1 wins with double pair of :ace and :3"
+           (double-pair-winner? (parse-hands {player1 "AS 7C AD 3S 3D", player2 "AS 2C AD 4S 3D"}))))
+    (is (= "Player 2 wins with double pair of :king and :3"
+           (double-pair-winner? (parse-hands {player1 "QS 7C QD 3S 3D", player2 "3S 2C 3D KS KD"}))))
+    (is (= "Player 2 wins with double pair of :queen and :5"
+           (double-pair-winner? (parse-hands {player1 "QS 7C QD 3S 3D", player2 "5S 2C 5D QS QD"}))))
+    (is (= "Player 1 wins with a double pair of :queen and :5 and highest card :7 of :club"
+           (double-pair-winner? (parse-hands {player1 "QS 7C QD 5C 5H", player2 "5S 2C 5D QH QC"}))))
+    (is (= "Player 1 and Player 2 win with a double pair of :queen and :5"
+           (double-pair-winner? (parse-hands {player1 "QS 7C QD 5C 5H", player2 "5S 7S 5D QH QC"}))))))
+
+
+
+(deftest winning-test
+
+  (testing "Winning with highest card"
+    (is (= (winner? {player1 "AS QD 3D TD 5S",
+                     player2 "AC QH 3C TH 9C",
+                     player3 "AD QC 5H JH 7C"})
+           "Player 3 wins with highest card :jack of :heart")))
+
+  (testing "Winning with pair"
+    (is (= (winner? {player1 "2S QD 5D 3D 5S", player2 "AC QH 3C TH 9C"})
+           "Player 1 wins with a pair of :5")))
+
+  (testing "Winning with double pair"
+    (is (= (winner? {player1 "2S QD 5D 3D 5S", player2 "AC 3H TC TH 3C"})
+           "Player 2 wins with double pair of :10 and :3")))
+
+  (testing "Winning with three of a kind"
+    (is (= (winner? {player1 "3H QD 5D 3D 3S", player2 "AC 5H TC TH 5C"})
+           "Player 1 wins with a three of kind of :3")))
+
+  (testing "Winning with straight"
+    (is (= (winner? {player1 "3H QD 5D 3D 3S", player2 "3C 5H 4C 7H 6C"})
+           "Player 2 wins with a straight of :heart at rank :7")))
+
+  (testing "Winning with flush"
+    (is (= (winner? {player1 "3S QS 5S 8S TS", player2 "3C 5H 4C 7H 6C"})
+           "Player 1 wins with a flush of :spade at rank :queen")))
+
+  (testing "Winning with full house"
+    (is (= (winner? {player1 "3C QS 5S 8D TH", player2 "3S 3H QC QH QS"})
+           "Player 2 wins with a full at :queen and :3")))
+
+  (testing "Winning with a four of a kind"
+    (is (= (winner? {player1 "5C 5S 7S 5D 5H", player2 "3S 3H QC QH QS"})
+           "Player 1 wins with a four of kind of :5")))
+
+  (testing "Winning with a straight flush"
+    (is (= (winner? {player1 "5C 5S 7S 5D 5H", player2 "6H 7H TH 9H 8H"})
+           "Player 2 wins with a straight flush of :heart at rank :10")))
+
   )
